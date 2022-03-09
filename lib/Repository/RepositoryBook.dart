97b3cf7e1bbbb48f304350020cab1317de1e2c2b
zayedelfasa <si.elfasa@gmail.com>
2022-03-09 22:56:02 +0800

    [update] bug fix code.

--- lib/Repository/RepositoryBook.dart
+++ lib/Repository/RepositoryBook.dart
@@ -4,29 +4,10 @@ import 'package:kelas_widget/serviceDio/ApiClient.dart';
 class RepositoryBook {
   Future<Response> findAllBook() async {
     try {
-      Response response = await ApiClient().dio.get("api/rest/book-category/findAll");
-      // "https://gloshonline.herokuapp.com/api/rest/book-category/findAll"
+      Response response = await ApiClient().dio.get("79dccac2-d2f0-423b-b19d-94705a3b9932");
       return response;
     } catch(err) {
       print('${err.toString()}');
     }
   }
-
-  Future<Response> findBookById() async {
-    try {
-      Response response = await Dio(
-        BaseOptions(
-          baseUrl: "",
-          headers: {
-
-          }
-        )
-      ).get("https://run.mocky.io/v3/b9b7d923-4603-4cf5-88d9-345d4eec0663");
-      return response;
-    } catch(err) {
-      print('${err.toString()}');
-    }
-  }
-
-  
 }
--- lib/serviceDio/ApiClient.dart
+++ lib/serviceDio/ApiClient.dart
@@ -4,8 +4,8 @@ import 'package:kelas_widget/service/GetxProvider/UserProvider.dart';
 class ApiClient {
   final Dio dio = Dio(
     BaseOptions(
-      // baseUrl: "https://run.mocky.io/v3/",
-      baseUrl: "https://gloshonline.herokuapp.com/",
+      baseUrl: "https://run.mocky.io/v3/",
+      // baseUrl: "https://gloshonline.herokuapp.com/",
       headers: {
         "Authentication" : "Bearer 1234567890",
         "Username" : "zayedelfasa"
